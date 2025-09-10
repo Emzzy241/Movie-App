790190f38896f3d10cce6f8b5c36c14f201f44b8
Emzzy241 <emzzyoluwole@gmail.com>
2025-09-10 13:05:58 +0100

    update: correcting the actual route to the OMDB API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg"
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 
 console.log(process.env.REACT_APP_MOVIE_API_KEY)
 
